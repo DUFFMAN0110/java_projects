ecb8c13ac9a1c494b5a5c68b65216276a7832e9d
DUFFMAN0110 <joseph.t.duff@gmail.com>
2023-09-22 23:18:23 -0500

    Adding runner for the Spiral class

---
+++ SpiralRunner.java
@@ -0,0 +1,13 @@
+import static java.lang.System.*; 
+import java.util.Arrays; 
+
+public class SpiralRunner 
+{
+    public static void main ( String [] args )
+    {
+       Spiral spin = new Spiral(5); 
+       out.print(spin); 
+    
+    }
+}
+
