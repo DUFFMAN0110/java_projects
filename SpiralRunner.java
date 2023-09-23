e1527773dcbd7a5844b1416672489160d70da2fb
DUFFMAN0110 <joseph.t.duff@gmail.com>
2023-09-23 01:56:58 -0500

    hasn't changed

--- SpiralRunner.java
+++ SpiralRunner.java
@@ -5,9 +5,9 @@ public class SpiralRunner
 {
     public static void main ( String [] args )
     {
-       Spiral spin = new Spiral(5); 
+       Spiral spin = new Spiral(4); 
        out.print(spin); 
     
     }
 }
-// random comment 
+
