52879839c333cd4d6a8962d8c793041a5340df96
DUFFMAN0110 <joseph.t.duff@gmail.com>
2023-10-05 23:57:39 -0500

    mas

---
+++ Testing_Java/SpiralRunnerTwo.java
@@ -0,0 +1,19 @@
+package Testing_Java;
+import static java.lang.System.*; 
+import java.util.Scanner;
+
+
+
+public class SpiralRunnerTwo 
+{
+    public static void main ( String [] args )
+    {
+        
+        Spiral spin = new Spiral(5); 
+        out.print("\n\n" + spin); 
+        spin = new Spiral(6);
+        out.println("\n\n" + spin);
+    } 
+
+}
+
